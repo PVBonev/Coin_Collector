b0ebb1bfe23d55ddf76afc6dccc758840b4583fd
Petko Bonev <9mi0800149@g.fmi.uni-sofia.bg>
2026-02-12 23:25:02 +0200

    Improved footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,9 +2,10 @@
     <div class="footer-inner">
         <div class="footer-info">
             <p> <?php echo date('Y'); ?> <strong>Coin Collector Project for FMI course WEB Technologies</strong></p>
-            <p>Created by: <strong>Petko Bonev</strong> | Email: <a href="petkovbonev@gmail.com">petkovbonev@gmail.com</a></p>
+            <p>Created by: <strong>Petko Bonev</strong> | Email: <a href="mailto:petkovbonev@gmail.com">petkovbonev@gmail.com</a></p>
         </div>
         <div class="footer-credits">
+            <p>Course Lecturer: <strong>Prof. Dr. Milen Petrov</strong></p>
             <p>Special thanks to <strong>Gabriella Kioseva</strong> for logo design help</p>
         </div>
     </div>
